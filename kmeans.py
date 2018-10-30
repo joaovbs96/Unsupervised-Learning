4f72ba03b72fb72e6f1378d6449d4cee34b8ca70
Tamara Campos <tamaramcampos02@gmail.com>
2018-10-29 23:26:24 -0300

    first implementation of kmeans

---
+++ kmeans.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import pandas as pd
+import sys
+from scipy.spatial.distance import cdist
+
+
+# disable SettingWithCopyWarning warnings
+pd.options.mode.chained_assignment = None  # default='warn'
+
+filename = sys.argv[1]
+data = pd.read_csv(filename)
+
+# number of centroids
+krange = range(1,30)
+
+# main loop to calculate k-means for all the centroids
+scores = []
+for k in krange:
+    km = KMeans(n_clusters=k,         # Number of centroids
+                init='k-means++',     # Centroids initialized as long as possible from each other
+                max_iter=200,         # Number of iterations - default 300
+                n_init=5)             # Runs kmeans 5 times with different initialization
+
+    km.fit(data)
+    #scores.append(km.inertia_) 
+    scores.append(np.average(np.min(cdist(data, km.cluster_centers_, 'euclidean'), axis=1)))
+   #scores.append(-km.score(data))       
+
+
+# Elbow curve
+plt.plot(krange, scores)
+plt.xlabel("K")
+plt.xticks(krange)
+plt.ylabel("Inertia Score")
+plt.show()
