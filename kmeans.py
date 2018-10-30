6c4a1e68d5651609433cb9a499245dcfe0c8ec1c
João Vítor Silva <joaovbs96@gmail.com>
2018-10-30 17:01:27 -0300

    printing inertia

--- kmeans.py
+++ kmeans.py
@@ -13,24 +13,25 @@ filename = sys.argv[1]
 data = pd.read_csv(filename)
 
 # number of centroids
-krange = range(1,30)
+krange = 16
 
 # main loop to calculate k-means for all the centroids
 scores = []
-for k in krange:
+for k in range(1, krange):
     km = KMeans(n_clusters=k,         # Number of centroids
                 init='k-means++',     # Centroids initialized as long as possible from each other
-                max_iter=200,         # Number of iterations - default 300
-                n_init=5)             # Runs kmeans 5 times with different initialization
+                max_iter=500,         # Number of iterations - default 300
+                n_init=10)             # Runs kmeans 10 times with different initialization
 
     km.fit(data)
-    #scores.append(km.inertia_) 
+    #scores.append(km.inertia_)
     scores.append(np.average(np.min(cdist(data, km.cluster_centers_, 'euclidean'), axis=1)))
-   #scores.append(-km.score(data))       
+    #scores.append(-km.score(data))
+    print(str(k) + ': ' + str(km.inertia_))
 
 
 # Elbow curve
-plt.plot(krange, scores)
+plt.plot([i for i in range(10)], scores)
 plt.xlabel("K")
 plt.xticks(krange)
 plt.ylabel("Inertia Score")
