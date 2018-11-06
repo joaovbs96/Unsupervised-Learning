a6a9d084bbb163dea632c1c57ef9ff3378cff89c
Tamara Campos <tamaramcampos02@gmail.com>
2018-11-06 00:28:54 -0200

    separating code

--- kmeans.py
+++ kmeans.py
@@ -24,9 +24,6 @@ clusters = {}
 labels = None
 for k in krange:
     km = KMeans(n_clusters=k)         # Number of centroids
-                #init='k-means++')     # Centroids initialized as long as possible from each other
-                #max_iter=700,         # Number of iterations - default 300
-                #n_init=5)            # Runs kmeans 10 times with different initialization
 
     # fit model to dataset
     km.fit(data)
@@ -41,29 +38,6 @@ for k in krange:
 plt.plot(krange, scores)
 plt.xlabel("Número de clusters")
 plt.ylabel("SSE")
-plt.savefig("kmeansP_20.png")
-
-# separate items on their clusters
-clusters_temp = {}
-for item in range(len(labels)):
-    if labels[item] in clusters_temp:
-        clusters_temp[labels[item]].append(item)
-    else:
-        clusters_temp[labels[item]] = [item]
-clusters = clusters_temp
-
-# read headlines
-textname = sys.argv[2]
-with open(textname, encoding="utf-8") as f:
-    content = f.readlines()
-content = [x.strip() for x in content]
-
-# print a few headlines of each cluster
-print(file=open("output.txt", "w", encoding="utf-8"))
-for i in clusters.keys():
-    for _ in range(5):
-        print(content[random.choice(clusters[i])], file=open("output.txt", "a", encoding="utf-8"))
-    print(file=open("output.txt", "a", encoding="utf-8"))
-    print(file=open("output.txt", "a", encoding="utf-8"))
+plt.savefig("kmeans.png")
 
     
--- pca.py
+++ pca.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from scipy.spatial.distance import cdist
 from sklearn.decomposition import PCA
+from sklearn.metrics import silhouette_score
+from sklearn import metrics
 
 # disable SettingWithCopyWarning warnings
 pd.options.mode.chained_assignment = None  # default='warn'
@@ -16,16 +18,17 @@ dataname = sys.argv[1]
 data = pd.read_csv(dataname, header=None)
 
 # number of centroids
-best_n_clusters = 80
+best_n_clusters = 4
 
 # dimensionality before pca
-print("Dimensãoes antes de pca: ", data.shape[1])
+print("Dimensões antes de pca: ", data.shape[1])
 
 # test a range of variances
 variances = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
 
 # main loop to calculate k-means for all the centroids
 scores = []
+silhouetteCoef = []
 clusters = {}
 labels = None
 for variance in variances:
@@ -34,18 +37,16 @@ for variance in variances:
 
     print("Mantendo ", variance, " de variância, temos: ", data_pca.shape[1], " features")
 
-    km = KMeans(n_clusters=best_n_clusters,      # Number of centroids
-                init='k-means++',               # Centroids initialized as long as possible from each other
-                max_iter=500,                   # Number of iterations - default 300
-                n_init=10)                      # Runs kmeans 10 times with different initialization
+    km = KMeans(n_clusters=best_n_clusters)      # Number of centroids
 
     # fit model to dataset
     km.fit(data_pca)
-    scores.append(km.inertia_ / data.shape[0])
-    
+    km.predict(data_pca)
+    labels = km.labels_
 
-    # run model on dataset
-    #labels = km.predict(data)
+    scores.append(sum(np.min(cdist(data_pca, km.cluster_centers_, 'euclidean'), axis=1)) / data_pca.shape[0])
+    silhouetteCoef.append(silhouette_score(data_pca, labels))
+    
 
 # Elbow curve
 plt.plot(variances, scores)
@@ -53,29 +54,14 @@ plt.xlabel("Variância")
 plt.ylabel("SSE")
 plt.xticks(variances)
 plt.title("Kmeans para " + str(best_n_clusters) + " clusters utilizando PCA")
-plt.savefig("pca.png")
+plt.savefig("pcaSSE.png")
 
-# separate items on their clusters
-#clusters_temp = {}
-#for item in range(len(labels)):
-#    if labels[item] in clusters_temp:
-#        clusters_temp[labels[item]].append(item)
-#    else:
-#        clusters_temp[labels[item]] = [item]
-#clusters = clusters_temp
+plt.clf()
 
-# read headlines
-#textname = sys.argv[2]
-#with open(textname, encoding="utf-8") as f:
-#    content = f.readlines()
-#content = [x.strip() for x in content]
-
-# print a few headlines of each cluster
-#print(file=open("output.txt", "w", encoding="utf-8"))
-#for i in clusters.keys():
-#    for _ in range(5):
-#        print(content[random.choice(clusters[i])], file=open("output.txt", "a", encoding="utf-8"))
-#    print(file=open("output.txt", "a", encoding="utf-8"))
-#    print(file=open("output.txt", "a", encoding="utf-8"))
-
-    
+# Silhouette coef
+plt.plot(variances, silhouetteCoef)
+plt.xlabel("Variância")
+plt.ylabel("Coeficiente de silhueta")
+plt.xticks(variances)
+plt.title("Kmeans para " + str(best_n_clusters) + " clusters utilizando PCA")
+plt.savefig("pcaCS.png")
